d8e2e279d7bf25002d820d4efcfca93080d7cff8
chuthanhphong <81786056+chuthanhphong@users.noreply.github.com>
2021-09-01 02:36:29 +0700

    phong git 2:36

--- src/back-end/src/main/java/com/codegym/casemodule6/service/userService/UserService.java
+++ src/back-end/src/main/java/com/codegym/casemodule6/service/userService/UserService.java
@@ -6,6 +6,7 @@ import com.codegym.casemodule6.security.userprincal.UserPrinciple;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -14,6 +15,7 @@ import java.util.Optional;
 public class UserService implements IUserService{
     @Autowired
     private IUserRepository iUserRepository;
+
     @Override
     public Iterable<User> findAll() {
         return iUserRepository.findAll();
@@ -26,6 +28,7 @@ public class UserService implements IUserService{
 
     @Override
     public User save(User user) {
+
         return iUserRepository.save(user);
     }
 
