23624c92eadeb6edc8c4bb837c8a0770a6a99ec2
dinhhuychuong3005 <dinhhuychuong3005@gmail.com>
2021-09-05 00:01:55 +0700

    commit trc pull

--- src/back-end/src/main/java/com/codegym/casemodule6/repository/IRoleRepository.java
+++ src/back-end/src/main/java/com/codegym/casemodule6/repository/IRoleRepository.java
@@ -1,6 +1,7 @@
 package com.codegym.casemodule6.repository;
 
 import com.codegym.casemodule6.model.entity.Role;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,6 @@ import java.util.Optional;
 
 @Repository
 public interface IRoleRepository extends PagingAndSortingRepository<Role,Long> {
+
     Optional<Role> findByName(String name);
 }
